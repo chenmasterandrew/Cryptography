bea4a1b8682a1c515b54d35a4180c766e1b4d15b
chenmasterandrew <30731383+chenmasterandrew@users.noreply.github.com>
2018-09-22 13:00:45 -0400

    Updated from Brython Server: 9/22/2018 1:00:45 PM

--- cryptography.py
+++ cryptography.py
@@ -1,7 +1,7 @@
 """
 cryptography.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Andrew Chen
+Credit: https://github.com/chenmasterandrew/Cryptography
 
 Assignment:
 
@@ -10,3 +10,41 @@ Write and submit a program that encrypts and decrypts user data.
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+def keymaster(keyindex, numkey):
+    if keyindex > numkey:
+        keyindex = 0
+    return keyindex
+
+def crypt(opt, message, key):
+    nummessage = []
+    numkey = []
+    index = 0
+    for char in message:
+        nummessage.append(associations.find(char))
+    for char in key:
+        numkey.append(associations.find(char))
+    for x in range(0,len(nummessage)):
+        
+        keyindex = keymaster(index, len(numkey)-1)
+        if opt == "e":
+            nummessage[index] += numkey[keyindex]
+            if nummessage[index] > 84:
+                nummessage[index] -= 85
+        else:
+            nummessage[index] -= numkey[keyindex]
+        index += 1
+    for num in nummessage:
+        print (associations[num], end = "")
+
+def chooseopt():
+    opt = input("Enter e to encrypt, d to decrypt, or q to quit: ")
+    if opt == "e" or opt == "d":
+        message = str(input("Message: "))
+        key = str(input("Key: "))
+        crypt(opt, message, key)
+    elif opt == "q":
+        print ("Goodbye!")
+    else:
+      print ("Did not understand command, try again.")
+      chooseopt()
+chooseopt()
